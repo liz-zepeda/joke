81e4fb25e76a4f707e3e58dcb2ae9e2a9b2c6908
Liz Zepeda <karla.zepeda@sprint.com>
2020-03-25 11:33:10 -0400

    custom directives

--- src/app/card-hover.directive.ts
+++ src/app/card-hover.directive.ts
@@ -1,14 +1,31 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appCardHover]'
 })
 export class CardHoverDirective {
+  @HostBinding('class.card-outline-primary') private isHovering: boolean = false;
+
+  @Input('appCardHover') config =  {
+    querySelector : '.card-text'
+  }
 
   constructor(private el: ElementRef,
               private renderer: Renderer) {
-  renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'gray');
+  // renderer.setElementStyle(el.nativeElement, 'backgroundColor', 'gray');
   // el.nativeElement.style.backgroundColor = 'gray';
+  
+  }
 
+  @HostListener('mouseover') onMouseOver() {
+    const punchlineEl = this.el.nativeElement.querySelector(this.config.querySelector);
+    this.renderer.setElementStyle(punchlineEl, 'display', 'block');
+    this.isHovering = true;
+  }
+
+  @HostListener('mouseout') onMouseOut() {
+    const punchlineEl = this.el.nativeElement.querySelector(this.config.querySelector);
+    this.renderer.setElementStyle(punchlineEl, 'display', 'none');
+    this.isHovering =  false;
   }
 }
