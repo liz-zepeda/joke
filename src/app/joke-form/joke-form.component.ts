ffff771a215b89ded6ab4f6036f0acccb2a01413
Liz Zepeda <karla.zepeda@sprint.com>
2020-03-26 13:27:16 -0400

    Model driven data validation

--- src/app/joke-form/joke-form.component.ts
+++ src/app/joke-form/joke-form.component.ts
@@ -1,16 +1,35 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Joke } from 'app/joke';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-joke-form',
   templateUrl: './joke-form.component.html',
   styleUrls: ['./joke-form.component.css']
 })
-export class JokeFormComponent  {
+export class JokeFormComponent extends OnInit {
   @Output() jokeCreated = new EventEmitter<Joke>();
+  myForm: FormGroup;
+  setup: FormControl;
+  punchline: FormControl;
 
+  ngOnInit() {
+   this.createControls();
+   this.createForm();
+  }
 
-  constructor() { }
+  createControls() {
+    this.setup = new FormControl('', Validators.required);
+    this.punchline = new FormControl('', Validators.required);
+  }
+
+  createForm() {
+    this.myForm = new FormGroup({
+      setup: this.setup,
+      punchline: this.punchline
+    });
+  }
+
   createJoke(setup: string, punchline: string) {
     this.jokeCreated.emit (new Joke(setup, punchline));
   }
